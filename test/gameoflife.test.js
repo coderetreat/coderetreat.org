44ee85d5a31250890801f6e8195cfe42d349c927
CarolineClark <5319573+CarolineClark@users.noreply.github.com>
2020-05-16 14:48:10 +0100

    gam of life: second failing test

--- test/gameoflife.test.js
+++ test/gameoflife.test.js
@@ -1,21 +1,30 @@
 // a living cell with 2-3 living neighbours lives
 // a dead cell with exactly 3 neighbours lives
-// a living cell with 4+ neighbours dies
 // a living cell with 1 or fewer living neighbours dies
-
-// given when then
+// a living cell with 4-8 neighbours dies
 
 describe("the standard rules", () => {
-  it("a living cell with 3 living neighbours lives", () => {
-    let isCellAlive = true;
-    let numOfLivingNeighbours = 3;
+  describe("a living cell with 2-3 living neighbours lives", () => {
+    it("3 living neighbours", () => {
+      let livingCell = true;
+      let numOfLivingNeighbours = 3;
 
-    let result = isCellAliveInNextGeneration(
-      isCellAlive,
-      numOfLivingNeighbours
-    );
+      let result = isCellAliveInNextGeneration(
+        livingCell,
+        numOfLivingNeighbours
+      );
 
-    expect(result).toEqual(true);
+      expect(result).toEqual(true);
+    });
+  });
+
+  it("a living cell with 1 or fewer living neighbours dies", () => {
+    let livingCell = true;
+    let numOfLivingNeighbours = 1;
+
+    let result = isCellAliveInNextGeneration(livingCell, numOfLivingNeighbours);
+
+    expect(result).toEqual(false);
   });
 });
 
