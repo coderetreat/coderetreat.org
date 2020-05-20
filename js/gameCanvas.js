9bd09a06d383a39a332bc97a6dc46b21999929be
Caroline Clark <5319573+CarolineClark@users.noreply.github.com>
2020-05-20 21:00:29 +0100

    WIP - Add a tick that happens every second

--- js/gameCanvas.js
+++ js/gameCanvas.js
@@ -1,6 +1,9 @@
 import * as PIXI from "pixi.js";
 
 const toRgb = (r, g, b) => {
+  r = r * 256 | 0;
+  b = b * 256 | 0;
+  g = g * 256 | 0;
   let color = (r % 256) * 256 * 256 + (g % 256) * 256 + (b % 256);
   return color;
 };
@@ -26,24 +29,37 @@ var graphicsGrid = grid.map((row) => {
   return row.map((el) => {
     var square = new PIXI.Graphics();
     app.stage.addChild(square);
-    return [el, square];
+    return square;
   });
 });
 
-app.ticker.speed = 0.001;
+const computeNextGeneration = () => {
+  console.log("Next gen");
+}
 
-let timePassed = 0;
-
-// Listen for frame updates
-app.ticker.add((delta) => {
-  timePassed = (timePassed + delta * 1000 | 0);
+const drawGeneration = () => {
   for (let y = 0; y < graphicsGrid.length; y++) {
     for (let x = 0; x < graphicsGrid[0].length; x++) {
-      var square = graphicsGrid[y][x][1];
-      var color = toRgb(timePassed, timePassed, timePassed);
+      
+      var square = graphicsGrid[y][x];
+      var color = toRgb(1, 1, 1);
       square.clear();
       square.beginFill(color);
       square.drawRect(x * 20, y * 20, 10, 10);
     }
+  }
+}
+
+
+let secondsPassed = 0;
+let nextSecond = 1;
+
+// Listen for frame updates
+app.ticker.add((delta) => {
+  secondsPassed = (secondsPassed + delta/100);
+  if(secondsPassed > nextSecond) {
+    computeNextGeneration();
+    drawGeneration();
+    nextSecond++;
   }
 });
