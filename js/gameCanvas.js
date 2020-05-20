ea3ec07a74386fa7b57c307cee4a17854f6106ec
Caroline Clark <5319573+CarolineClark@users.noreply.github.com>
2020-05-20 23:09:48 +0100

    Game of life implemented on banner

--- js/gameCanvas.js
+++ js/gameCanvas.js
@@ -1,5 +1,5 @@
 import * as PIXI from "pixi.js";
-import { computeNextGeneration } from "./gameOfLife/computeNextGen";
+import { computeNextGeneration } from "./gameOfLife/computeNextGeneration";
 
 const toRgb = (r, g, b) => {
   r = r * 256 | 0;
@@ -14,7 +14,6 @@ const view = document.getElementById("gameCanvas");
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container
 
-let { width, height } = view.parentElement.getBoundingClientRect();
 const app = new PIXI.Application({
   view,
   resizeTo: view.parentElement,
@@ -25,31 +24,34 @@ const cellsContainer = new PIXI.Graphics();
 app.stage.addChild(cellsContainer);
 const drawGeneration = (currentGrid) => {
   cellsContainer.clear();
-  for (let y = 0; y < currentGrid.length; y++) {
+  var color = 0xFFFFFF;
+  cellsContainer.beginFill(color);
+for (let y = 0; y < currentGrid.length; y++) {
     for (let x = 0; x < currentGrid[0].length; x++) {
       var isCellAlive = currentGrid[y][x] === 1;
       if(!isCellAlive) continue;
-      var color = 0xFFFFFF;
-      cellsContainer.beginFill(color);
-      cellsContainer.drawRect(x * 20, y * 20, 10, 10);
+      cellsContainer.drawCircle(x * 10, y * 10, 4);
     }
   }
 }
 
-
 let secondsPassed = 0;
 let nextSecond = 1;
-let grid = [
-  [1, 1, 1, 1],
-  [1, 1, 0, 1],
-];
+
+let { width, height } = app.screen;
+let gridX = width/10 | 0;
+let gridY = height/10 | 0;
+
+let grid = Array(gridY).fill(0).map(() => Array(gridX).fill(0).map(() => Math.random() > 0.5 ? 1 : 0));
+drawGeneration(grid);
 
 // Listen for frame updates
 app.ticker.add((delta) => {
-  secondsPassed = (secondsPassed + delta/100);
-  if(secondsPassed > nextSecond) {
+  secondsPassed += delta/50;
+  if (secondsPassed > nextSecond) {
     grid = computeNextGeneration(grid);
     drawGeneration(grid);
     nextSecond++;
   }
+  
 });
--- test/countNeighbours.test.js
+++ test/countNeighbours.test.js
@@ -2,11 +2,17 @@ import {countNeighbours } from "../js/gameOfLife/countNeighbours";
 
 describe("count neighbours", () => {
     let grid = [
-        [0, 0, 1],
-        [0, 1, 1],
-        [0, 1, 1],
+        [1, 0, 1],
+        [1, 1, 1],
+        [1, 1, 1],
     ] 
     it("the center tile counts all 8 neighbours", () => {
-        expect(countNeighbours(grid, {y: 1, x: 1})).toEqual(4);
+        expect(countNeighbours(grid, {y: 1, x: 1})).toEqual(7);
+    })
+    it("the edge tile count 5 neighbours", () => {
+        expect(countNeighbours(grid, {y: 2, x: 1})).toEqual(5);
+    })
+    it("the edge tile count 5 neighbours", () => {
+        expect(countNeighbours(grid, {y: 0, x: 1})).toEqual(5);
     })
 })
