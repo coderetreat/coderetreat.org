62c0db1523baa9de7561acd6b6eaaf842aec938d
Raimo Radczewski <raimoradczewski@googlemail.com>
2020-05-28 13:59:40 +0200

    Add more visuals and reduced motion query

--- js/gameCanvas.js
+++ js/gameCanvas.js
@@ -7,13 +7,31 @@ const ALPHA_DELTA = 0.01;
 const SEED_THRESHOLD = 0.7;
 
 const view = document.getElementById("gameCanvas");
+const reducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+
 const app = new PIXI.Application({
   view,
   resizeTo: view.parentElement,
   antialias: true,
   autoDensity: true,
   resolution: 2,
+  autoStart: !reducedMotion
 });
+
+document.querySelector(`#jumbotron-gol-control-pause ${reducedMotion ? ".fa-pause" : ".fa-play"}`).classList.add("d-none");
+document.querySelector("#jumbotron-gol-control-pause").addEventListener("click", (e) => {
+  e.preventDefault();
+  const shouldStart = !app.ticker.started;
+  if(shouldStart) {
+    app.start();
+  } else {
+    app.stop();
+  }
+  document.querySelector("#jumbotron-gol-control-pause [data-icon=pause]").classList.toggle("d-none");
+  document.querySelector("#jumbotron-gol-control-pause [data-icon=play]").classList.toggle("d-none");
+})
+
+
 
 let { width, height } = app.screen;
 let gridX = (width / CELL_SIZE) | 0;
@@ -33,7 +51,7 @@ let graphics = grid.map((row, y) =>
     point.interactive = true;
     point.x = x * CELL_SIZE;
     point.y = y * CELL_SIZE;
-    point.alpha = 0;
+    point.alpha = !reducedMotion ? 0 : grid[y][x];
     point.alphaDelta = grid[y][x] == 1 ? ALPHA_DELTA : -ALPHA_DELTA;
     point.beginFill(0xffffff);
     point.drawCircle(CELL_SIZE / 2, CELL_SIZE / 2, (CELL_SIZE - 2) / 2);
@@ -69,6 +87,8 @@ let nextSecond = 1;
 drawGeneration(0);
 updateAlphaDelta();
 let timeInMs = 0;
+
+if(reducedMotion) app.render();
 
 // Listen for frame updates
 app.ticker.add((delta) => {
