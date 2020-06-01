c8ebed15b9a4e3909b038fd7d2d13f8c70561d4e
Raimo Radczewski <raimoradczewski@googlemail.com>
2020-06-02 00:47:03 +0200

    Add first GameController init stuff

--- js/gameOfLifeJumbotron/GameController.test.js
+++ js/gameOfLifeJumbotron/GameController.test.js
@@ -1,26 +1,59 @@
 /**
  * @jest-environment jsdom
  */
-import { GameOfLife, StandardRules, X, O } from "./GameOfLife";
 import { GameController } from "./GameController";
-import { Array2d } from "./Array2d";
+import { GraphicsController } from "./GraphicsController";
+import { GameOfLife, StandardRules } from "./GameOfLife";
+jest.mock("./GraphicsController");
+jest.mock("./GameOfLife");
 
 describe("GameController", () => {
   let element;
+
   beforeEach(() => {
+    jest.clearAllMocks();
+    const container = document.createElement("div");
     element = document.createElement("canvas");
+    container.appendChild(element);
   });
 
-  it("advances the game every tick", () => {
-    const game = new GameOfLife(new Array2d([[X]]), StandardRules);
-    const controller = new GameController({
-      game,
-      element,
-      tick: 1000,
+  it("initializes the GraphicsController on the given canvas", () => {
+    const controller = new GameController(element);
+
+    expect(GraphicsController).toHaveBeenCalledTimes(1);
+    expect(GraphicsController.mock.calls[0][0]).toMatchObject({ element });
+  });
+
+  describe("Game initialization", () => {
+    it("uses the ?seed and &rules parameters to initialize the Game if present", () => {
+      window.history.replaceState({}, "", "?seed=hello&rules=S2B2");
+
+      const controller = new GameController(element);
+      controller.initializeGame();
+
+      expect(GameOfLife.fromSeed).toHaveBeenCalledWith("hello", 0.7, 100, 100, {
+        born: [2],
+        survive: [2],
+      });
     });
 
-    controller.onTick(1000);
+    it("allows to specify a packed game value", () => {
+      const packed = "B3|S23|W1|H1|GAA==";
+      window.history.replaceState({}, "", "?state="+encodeURIComponent(packed));
 
-    expect(game.grid.backing).toEqual([[O]]);
+      const controller = new GameController(element);
+      controller.initializeGame();
+
+      expect(GameOfLife.fromPacked).toHaveBeenCalledWith(packed);
+    });
+
+    it("will otherwise randomly initialize the game", () => {
+      window.history.replaceState({}, "", "/");
+
+      const controller = new GameController(element);
+      controller.initializeGame();
+
+      expect(GameOfLife.fromSeed).toHaveBeenCalledWith(expect.anything(), 0.7, 100, 100, StandardRules);
+    });
   });
 });
--- js/gameOfLifeJumbotron/GameController.ts
+++ js/gameOfLifeJumbotron/GameController.ts
@@ -1,30 +1,46 @@
-import { GameOfLife } from "./GameOfLife";
+import { GameOfLife, StandardRules } from "./GameOfLife";
+import * as qs from "qs";
+import { GraphicsController } from "./GraphicsController";
 
 export class GameController {
-  game: GameOfLife;
-  element: any;
-  tick: any;
-  timeSinceLastTick: number = 0;
+  canvas: HTMLCanvasElement;
+  graphicsController: GraphicsController;
 
-  constructor({
-    game,
-    element,
-    tick,
-  }: {
-    game: GameOfLife;
-    element: HTMLElement;
-    tick: number;
-  }) {
-    this.game = game;
-    this.element = element;
-    this.tick = tick;
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+
+    this.graphicsController = new GraphicsController({
+      element: canvas,
+      fps: 30,
+      radius: 20,
+      fadeFactor: false,
+      gap: 4,
+    });
   }
 
-  onTick(delta) {
-    this.timeSinceLastTick += delta;
-    if (this.timeSinceLastTick >= this.tick) {
-      this.game.tick();
-      this.timeSinceLastTick = this.timeSinceLastTick % this.tick;
+  initializeGame() {
+    const urlParams = qs.parse(window.location.search, {
+      ignoreQueryPrefix: true,
+    });
+    if (urlParams.seed) {
+      let rules = { born: [3], survive: [2, 3] };
+      if (urlParams.rules) {
+        let match = /S(\d+)B(\d+)/.exec(<string>urlParams.rules);
+        if (match) {
+          rules = {
+            survive: Array.from(match[1]).map(Number),
+            born: Array.from(match[2]).map(Number),
+          };
+        }
+      }
+      const probability = urlParams.probability
+        ? Number(urlParams.probability)
+        : 0.7;
+      GameOfLife.fromSeed(urlParams.seed, probability, 100, 100, rules);
+    } else if (urlParams.state) {
+      GameOfLife.fromPacked(<string>urlParams.state);
+    } else {
+      GameOfLife.fromSeed(String(Math.random()*100000 | 0), 0.7, 100, 100, StandardRules);
     }
   }
 }
