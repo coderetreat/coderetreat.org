5831433d68be06c181acfbf6bd24653cf79dfe11
Raimo Radczewski <raimoradczewski@googlemail.com>
2020-06-04 18:14:36 +0200

    Bundle ohne a few icons (still too much)

--- js/default.js
+++ js/default.js
@@ -1,6 +1,7 @@
 import jquery from "jquery/dist/jquery";
 import "popper.js/dist/esm/popper";
 import "bootstrap/dist/js/bootstrap";
-import "@fortawesome/fontawesome-free/js/all";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/brands";
 
 window.$ = jquery;
