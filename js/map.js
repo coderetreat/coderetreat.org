3e0eac98af5ef1b432207b18df93db148c17417d
Raimo Radczewski <raimoradczewski@googlemail.com>
2020-07-11 14:12:41 +0200

    Highlight all timzones with the same local time

--- js/map.js
+++ js/map.js
@@ -1,36 +1,47 @@
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import "leaflet.vectorgrid/dist/Leaflet.VectorGrid";
+import "proj4leaflet";
 import geo from "./map/ne_10m_time_zones.json";
 
-var mymap = L.map('map').setView([45, Math.random()*180], 2);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+const map = L.map("map").setView([45, Math.random() * 180], 2);
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     minZoom: 2,
-    id: 'rradczewski/ckcggc2xd0azq1iqd9t5f0yhk',
+    id: "rradczewski/ckcggc2xd0azq1iqd9t5f0yhk",
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoicnJhZGN6ZXdza2kiLCJhIjoiY2tjZ2cyenJqMGp1YzJ0bHBrOTR5dHlsdyJ9.6kI34USWMzJ3hxS7j946xg'
-}).addTo(mymap);
-
-console.log(geo);
-
-var vectorGrid = L.vectorGrid.slicer( geo, {
-  rendererFactory: L.svg.tile,
-  vectorTileLayerStyles: {
-    sliced: function(properties) {
-      return {
-        stroke: true,
-        opacity: 0.2,
-        weight: 0.5,
-      }
-    }
-  },
-  interactive: true,
-  getFeatureId: function(f) {
-    return f.properties.wb_a3;
+    accessToken:
+      "pk.eyJ1IjoicnJhZGN6ZXdza2kiLCJhIjoiY2tjZ2cyenJqMGp1YzJ0bHBrOTR5dHlsdyJ9.6kI34USWMzJ3hxS7j946xg",
   }
-})
+).addTo(map);
 
-vectorGrid.addTo(mymap);
+const timezones = new L.GeoJSON(geo, {
+  style: function (feature) {
+    return {
+      fill: true,
+      fillOpacity: 0,
+      stroke: true,
+      opacity: 0.2,
+    };
+  },
+  onEachFeature: (f, layer) => {
+    layer.on("mouseover", (e) => {
+      highlightFeaturesInSameTimezone(e.target, layer);
+    });
+  },
+});
+
+const highlightFeaturesInSameTimezone = (layer) => {
+  const allFeatureLayers = Object.values(map._layers).filter(layer => layer.feature && layer.feature.properties.featurecla === "Timezone");
+  allFeatureLayers.forEach(layer => timezones.resetStyle(layer));
+  const layersInSameTimezone = allFeatureLayers.filter(otherLayer => otherLayer.feature.properties.zone == layer.feature.properties.zone);
+  console.log(layersInSameTimezone);
+  layersInSameTimezone.forEach(layer => layer.setStyle({ opacity: 0.8 }));
+}
+
+timezones.addTo(map);
