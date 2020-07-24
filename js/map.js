7816b2501455ec25ebb1bf62816385aa6afa3f33
Raimo Radczewski <raimoradczewski@googlemail.com>
2020-07-24 11:26:37 +0200

    Load timezones after map tiles

--- js/map.js
+++ js/map.js
@@ -2,14 +2,13 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import "leaflet.vectorgrid/dist/Leaflet.VectorGrid";
 import "proj4leaflet";
-import geo from "./map/ne_10m_time_zones.json";
 
 const map = L.map("map", {
   maxZoom: 18,
   minZoom: 2,
 }).fitBounds([[45,-120], [45, 120]], { animate: false });
 
-L.tileLayer(
+const tiles = L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
     attribution:
@@ -22,29 +21,34 @@ L.tileLayer(
     accessToken:
       "pk.eyJ1IjoicnJhZGN6ZXdza2kiLCJhIjoiY2tjZ2cyenJqMGp1YzJ0bHBrOTR5dHlsdyJ9.6kI34USWMzJ3hxS7j946xg",
   }
-).addTo(map);
+);
+tiles.addTo(map);
 
-const timezones = new L.GeoJSON(geo, {
-  style: function (feature) {
-    return {
-      fill: true,
-      fillOpacity: 0,
-      stroke: true,
-      opacity: 0.2,
-    };
-  },
-  onEachFeature: (f, layer) => {
-    layer.on("mouseover", (e) => {
-      highlightFeaturesInSameTimezone(e.target, layer);
+tiles.once("load", () => {
+  import("./map/ne_10m_time_zones.json").then(geo => {
+    const timezones = new L.GeoJSON(geo, {
+      style: function (feature) {
+        return {
+          fill: true,
+          fillOpacity: 0,
+          stroke: true,
+          opacity: 0.2,
+        };
+      },
+      onEachFeature: (f, layer) => {
+        layer.on("mouseover", (e) => {
+          highlightFeaturesInSameTimezone(e.target, layer);
+        });
+      },
     });
-  },
-});
-
-const highlightFeaturesInSameTimezone = (layer) => {
-  const allFeatureLayers = Object.values(map._layers).filter(layer => layer.feature && layer.feature.properties.featurecla === "Timezone");
-  allFeatureLayers.forEach(layer => timezones.resetStyle(layer));
-  const layersInSameTimezone = allFeatureLayers.filter(otherLayer => otherLayer.feature.properties.zone == layer.feature.properties.zone);
-  layersInSameTimezone.forEach(layer => layer.setStyle({ opacity: 0.8 }));
-}
-
-timezones.addTo(map);
+    
+    const highlightFeaturesInSameTimezone = (layer) => {
+      const allFeatureLayers = Object.values(map._layers).filter(layer => layer.feature && layer.feature.properties.featurecla === "Timezone");
+      allFeatureLayers.forEach(layer => timezones.resetStyle(layer));
+      const layersInSameTimezone = allFeatureLayers.filter(otherLayer => otherLayer.feature.properties.zone == layer.feature.properties.zone);
+      layersInSameTimezone.forEach(layer => layer.setStyle({ opacity: 0.8 }));
+    }
+    
+    timezones.addTo(map);
+  });
+  });
