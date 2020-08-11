d2b8acd66ce10b5af8a81d0b9e1930d3febe1e2d
Raimo Radczewski <raimoradczewski@googlemail.com>
2020-08-11 23:37:52 +0200

    Fix styling

--- js/events.jsx
+++ js/events.jsx
@@ -1,48 +1,52 @@
 import { Fragment, render, h } from "preact";
-import { useState, useEffect } from 'preact/hooks';
-import 'regenerator-runtime/runtime';
+import { useState, useEffect, useRef } from "preact/hooks";
+import "regenerator-runtime/runtime";
 import EventCard from "./events/EventCard";
 
 const Events = () => {
-    const [eventList, setEventList] = useState([]);
+  const [eventList, setEventList] = useState([]);
 
-    useEffect(() => {
-        const Run = async () => {
-            const result = await fetch("/events/events.json")
-            setEventList(Object.values(await result.json()))
-        }
-        Run()
-    }, []);
+  const scrollSpy = useRef(null);
 
-    return (
-        <Fragment>
-            <div class="jumbotron jumbotron-fluid m-0 p-0">
-                <div class="container" style="line-height: 200%;align-content: ;">
-                    <h1 style="line-height: 3;font-size: 3rem;font-weight: 900;font-family: &quot;Arial Black&quot;, Helvetica;">I'm
-                        based in <select>
-                            <option>cyberspace</option>
-                        </select>.<br/> My Timezone is <select>
-                            <option>UTC-1</option>
-                        </select>. <br/>I want to attend an event that starts at <select>
-                            <option>9:00 AM</option>
-                        </select>
-                    </h1>
-                    <button>9 virtual events found</button>
-                    <button>10 on-site events found</button>
-                </div>
-            </div>
-            <div class="bg-light text-dark py-5">
-                <div style="overflow-x: scroll; white-space: nowrap;">
-                    <div style="display: inline-block">
-                        {eventList.map(e => <EventCard event={e}/>)}
-                    </div>
-                </div>
-            </div>
-        </Fragment>
-    );
+  useEffect(() => {
+    const Run = async () => {
+      const result = await fetch("/events/events.json");
+      setEventList(Object.values(await result.json()));
+    };
+    Run();
+  }, []);
+
+  return (
+    <Fragment>
+      <div class="jumbotron jumbotron-fluid m-0 p-0">
+        <div class="container" style={{ lineHeight: "200%" }}>
+          <h1 style='line-height: 3;font-size: 3rem;font-weight: 900;font-family: "Arial Black", Helvetica;'>
+            I'm based in{" "}
+            <select>
+              <option>cyberspace</option>
+            </select>
+            .<br /> My Timezone is{" "}
+            <select>
+              <option>UTC-1</option>
+            </select>
+            . <br />I want to attend an event that starts at{" "}
+            <select>
+              <option>9:00 AM</option>
+            </select>
+          </h1>
+          <button>9 virtual events found</button>
+          <button>10 on-site events found</button>
+        </div>
+      </div>
+      <div class="bg-light text-dark py-5">
+        <div ref={scrollSpy} style={{ overflowX: "scroll", whiteSpace: "nowrap" }}>
+          {eventList.map((e) => (
+            <EventCard event={e} />
+          ))}
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
-render(
-    <Events />,
-    document.querySelector("#events")
-);
+render(<Events />, document.querySelector("#events"));
--- js/events/EventCard.jsx
+++ js/events/EventCard.jsx
@@ -1,19 +1,24 @@
 import { render, h } from "preact";
-export default ({event}) => (
-
-<div class="card m-3" style="width: 18rem;  white-space: normal;">
-    <div class="card-body">
+export default ({ event }) => (
+  <div style={{ display: "inline-block" }}>
+    <div class="card m-3" style={{ width: "18rem", whiteSpace: "normal" }}>
+      <div class="card-body">
         <h5 class="card-title">{event.title}</h5>
         <p class="card-text">{event.description}</p>
-    </div>
-    <ul class="list-group list-group-flush">
+      </div>
+      <ul class="list-group list-group-flush">
         <li class="list-group-item">{event.format}</li>
         <li class="list-group-item">{event.spoken_language}</li>
         <li class="list-group-item">{event.location}</li>
-    </ul>
-    <div class="card-body">
-        <a href="#" class="card-link">{event.url}</a>
-        <a href="#" class="card-link">{event.moderators}</a>
+      </ul>
+      <div class="card-body">
+        <a href="#" class="card-link">
+          {event.url}
+        </a>
+        <a href="#" class="card-link">
+          {event.moderators}
+        </a>
+      </div>
     </div>
-</div>
+  </div>
 );
