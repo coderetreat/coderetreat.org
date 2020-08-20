eb43db05c4334f7b41213fd7d0031f99b7c48f0e
Matthew Plavcan <mwplavcan@gmail.com>
2020-08-20 14:16:35 -0700

    Implemented re-zoned event dates

--- js/events.jsx
+++ js/events.jsx
@@ -1,17 +1,30 @@
 import { Fragment, render, h } from "preact";
 import { useState, useEffect, useRef } from "preact/hooks";
+import * as jsjoda from "@js-joda/core";
 import "regenerator-runtime/runtime";
 import EventCard from "./events/EventCard";
+const { ZonedDateTime } = jsjoda;
 
 const Events = () => {
+  const zoneList = ["UTC−12:00","UTC−11:00","UTC−10:00","UTC−09:30","UTC−09:00","UTC−08:00","UTC−07:00","UTC−06:00","UTC−05:00","UTC−04:00","UTC−03:30","UTC−03:00","UTC−02:00","UTC−01:00","UTC±00:00","UTC+01:00","UTC+02:00","UTC+03:00","UTC+03:30","UTC+04:00","UTC+04:30","UTC+05:00","UTC+05:30","UTC+05:45","UTC+06:00","UTC+06:30","UTC+07:00","UTC+08:00","UTC+08:45","UTC+09:00","UTC+09:30","UTC+10:00","UTC+10:30","UTC+11:00","UTC+12:00","UTC+12:45","UTC+13:00","UTC+14:00"];
+  const timeList = ["9:00am"];
+  const [timeZone, setTimeZone] = useState(zoneList[0]);
+  const [startTime, setStartTime] = useState([]);
   const [eventList, setEventList] = useState([]);
-
   const scrollSpy = useRef(null);
 
   useEffect(() => {
     const Run = async () => {
       const result = await fetch("/events/events.json");
-      setEventList(Object.values(await result.json()).sort((a,b) => new Date(b.date.start) - new Date(a.date.start)))
+      setEventList(Object.values(await result.json())
+        .map(event => ({
+          ...event,
+          date: {
+            start : ZonedDateTime.parse(event.date.start),
+            end : ZonedDateTime.parse(event.date.end)
+          }
+        }))
+        .sort((a,b) => b.date.start.compareTo(a.date.start)))
     };
     Run();
   }, []);
@@ -21,13 +34,13 @@ const Events = () => {
       <div class="jumbotron jumbotron-fluid m-0 p-0">
         <div class="container" style={{ lineHeight: "200%" }}>
           <h1 style='line-height: 1.5;font-size: 3rem;font-weight: 900;font-family: "Arial Black", Helvetica;'>
-            My Timezone is{" "}
+            My timezone is{" "}
             <select>
-              <option>UTC-1</option>
+              {zoneList.map(z => <option>{z}</option>)}
             </select>
             . <br />I want to attend an event that starts at{" "}
             <select>
-              <option>9:00 AM</option>
+              {timeList.map(z => <option>{z}</option>)}
             </select>
           </h1>
           <button>9 virtual events found</button>
