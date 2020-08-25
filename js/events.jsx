15c6056f5445854acf586f82268f8f24252a8fb5
Raimo Radczewski <raimoradczewski@googlemail.com>
2020-08-25 21:30:19 +0200

    Sort correctly and scroll to the first matching event

--- js/events.jsx
+++ js/events.jsx
@@ -3,25 +3,25 @@ import { useState, useEffect, useRef } from "preact/hooks";
 import * as jsjoda from "@js-joda/core";
 import "regenerator-runtime/runtime";
 import EventCard from "./events/EventCard";
-const { ZonedDateTime } = jsjoda;
+const { ZonedDateTime, LocalDate, LocalTime, ZoneId } = jsjoda;
 
 const Events = () => {
   const zoneList = [
-    "UTC−12:00",
-    "UTC−11:00",
-    "UTC−10:00",
-    "UTC−09:30",
-    "UTC−09:00",
-    "UTC−08:00",
-    "UTC−07:00",
-    "UTC−06:00",
-    "UTC−05:00",
-    "UTC−04:00",
-    "UTC−03:30",
-    "UTC−03:00",
-    "UTC−02:00",
-    "UTC−01:00",
-    "UTC±00:00",
+    "UTC-12:00",
+    "UTC-11:00",
+    "UTC-10:00",
+    "UTC-09:30",
+    "UTC-09:00",
+    "UTC-08:00",
+    "UTC-07:00",
+    "UTC-06:00",
+    "UTC-05:00",
+    "UTC-04:00",
+    "UTC-03:30",
+    "UTC-03:00",
+    "UTC-02:00",
+    "UTC-01:00",
+    "UTC+00:00",
     "UTC+01:00",
     "UTC+02:00",
     "UTC+03:00",
@@ -46,9 +46,9 @@ const Events = () => {
     "UTC+13:00",
     "UTC+14:00",
   ];
-  const timeList = ["9:00am"];
+  const timeList = ["09:00"];
   const [timeZone, setTimeZone] = useState(zoneList[0]);
-  const [startTime, setStartTime] = useState([]);
+  const [startTime, setStartTime] = useState(timeList[0]);
   const [eventList, setEventList] = useState([]);
   const scrollContainer = useRef(null);
   const scrollSpy = useRef([]);
@@ -65,23 +65,31 @@ const Events = () => {
               end: ZonedDateTime.parse(event.date.end),
             },
           }))
-          .sort((a, b) => b.date.start.compareTo(a.date.start))
+          .sort((a, b) => a.date.start.compareTo(b.date.start))
       );
     };
     Run();
   }, []);
 
   useEffect(() => {
-    let i = 0;
-    const interval = setInterval(() => {
-      scrollContainer.current.scrollTo({
-        left: scrollSpy.current[(i++) % scrollSpy.current.length].base.offsetLeft,
-        behavior: "smooth",
-      });
-    }, 1000);
+    const time = LocalDate.parse("2019-11-16")
+      .atTime(LocalTime.parse(startTime))
+      .atZone(ZoneId.of(timeZone));
 
-    return () => clearInterval(interval);
-  }, []);
+    console.log(time.withZoneSameInstant(ZoneId.UTC).toString());
+
+    let i;
+    for(i = 0; i < eventList.length; i++) {
+      console.log("Testing", eventList[i].date.start.withZoneSameInstant(ZoneId.UTC).toString());
+      if(eventList[i].date.start.isEqual(time)) {
+        break;
+      }
+    }
+
+    console.log("Earliest event is", eventList[i]);
+    scrollContainer.current.scrollTo({ left: scrollSpy.current[i]?.base.offsetLeft, behavior: "smooth" });
+
+    }, [timeZone, startTime, eventList]);
 
   return (
     <Fragment>
@@ -89,15 +97,17 @@ const Events = () => {
         <div class="container" style={{ lineHeight: "200%" }}>
           <h1 style='line-height: 1.5;font-size: 3rem;font-weight: 900;font-family: "Arial Black", Helvetica;'>
             My timezone is{" "}
-            <select>
+            <select onChange={(e) => setTimeZone(e.target.value)}>
               {zoneList.map((z) => (
-                <option>{z}</option>
+                <option value={z} selected={z === timeZone}>
+                  {z}
+                </option>
               ))}
             </select>
             . <br />I want to attend an event that starts at{" "}
-            <select>
+            <select onChange={(e) => setStartTime(e.target.value)}>
               {timeList.map((z) => (
-                <option>{z}</option>
+                <option value={z}>{z}</option>
               ))}
             </select>
           </h1>
--- js/events/EventCard.jsx
+++ js/events/EventCard.jsx
@@ -1,4 +1,7 @@
 import { render, h } from "preact";
+import * as jsJoda from "@js-joda/core";
+const { ZoneId } = jsJoda;
+
 export default ({ event }) => (
   <div style={{ display: "inline-block" }}>
     <div class="card m-3" style={{ width: "18rem", whiteSpace: "normal" }}>
@@ -7,6 +10,8 @@ export default ({ event }) => (
         <p class="card-text">{event.description}</p>
       </div>
       <ul class="list-group list-group-flush">
+        <li class="list-group-item">{event.date.start.toString()}<br/>{event.date.start.withZoneSameInstant(ZoneId.UTC).toString()}</li>
+        <li class="list-group-item">{event.date.end.toString()}</li>
         <li class="list-group-item">{event.format}</li>
         <li class="list-group-item">{event.spoken_language}</li>
         <li class="list-group-item">{event.location}</li>
