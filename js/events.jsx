5dcc16046c1536b92b21e0b580e3a09700ea9092
Raimo Radczewski <raimoradczewski@googlemail.com>
2020-08-25 20:45:45 +0200

    Save refs to the cards and scroll through them as a proof of concept

--- js/events.jsx
+++ js/events.jsx
@@ -6,27 +6,81 @@ import EventCard from "./events/EventCard";
 const { ZonedDateTime } = jsjoda;
 
 const Events = () => {
-  const zoneList = ["UTC−12:00","UTC−11:00","UTC−10:00","UTC−09:30","UTC−09:00","UTC−08:00","UTC−07:00","UTC−06:00","UTC−05:00","UTC−04:00","UTC−03:30","UTC−03:00","UTC−02:00","UTC−01:00","UTC±00:00","UTC+01:00","UTC+02:00","UTC+03:00","UTC+03:30","UTC+04:00","UTC+04:30","UTC+05:00","UTC+05:30","UTC+05:45","UTC+06:00","UTC+06:30","UTC+07:00","UTC+08:00","UTC+08:45","UTC+09:00","UTC+09:30","UTC+10:00","UTC+10:30","UTC+11:00","UTC+12:00","UTC+12:45","UTC+13:00","UTC+14:00"];
+  const zoneList = [
+    "UTC−12:00",
+    "UTC−11:00",
+    "UTC−10:00",
+    "UTC−09:30",
+    "UTC−09:00",
+    "UTC−08:00",
+    "UTC−07:00",
+    "UTC−06:00",
+    "UTC−05:00",
+    "UTC−04:00",
+    "UTC−03:30",
+    "UTC−03:00",
+    "UTC−02:00",
+    "UTC−01:00",
+    "UTC±00:00",
+    "UTC+01:00",
+    "UTC+02:00",
+    "UTC+03:00",
+    "UTC+03:30",
+    "UTC+04:00",
+    "UTC+04:30",
+    "UTC+05:00",
+    "UTC+05:30",
+    "UTC+05:45",
+    "UTC+06:00",
+    "UTC+06:30",
+    "UTC+07:00",
+    "UTC+08:00",
+    "UTC+08:45",
+    "UTC+09:00",
+    "UTC+09:30",
+    "UTC+10:00",
+    "UTC+10:30",
+    "UTC+11:00",
+    "UTC+12:00",
+    "UTC+12:45",
+    "UTC+13:00",
+    "UTC+14:00",
+  ];
   const timeList = ["9:00am"];
   const [timeZone, setTimeZone] = useState(zoneList[0]);
   const [startTime, setStartTime] = useState([]);
   const [eventList, setEventList] = useState([]);
-  const scrollSpy = useRef(null);
+  const scrollContainer = useRef(null);
+  const scrollSpy = useRef([]);
 
   useEffect(() => {
     const Run = async () => {
       const result = await fetch("/events/events.json");
-      setEventList(Object.values(await result.json())
-        .map(event => ({
-          ...event,
-          date: {
-            start : ZonedDateTime.parse(event.date.start),
-            end : ZonedDateTime.parse(event.date.end)
-          }
-        }))
-        .sort((a,b) => b.date.start.compareTo(a.date.start)))
+      setEventList(
+        Object.values(await result.json())
+          .map((event) => ({
+            ...event,
+            date: {
+              start: ZonedDateTime.parse(event.date.start),
+              end: ZonedDateTime.parse(event.date.end),
+            },
+          }))
+          .sort((a, b) => b.date.start.compareTo(a.date.start))
+      );
     };
     Run();
+  }, []);
+
+  useEffect(() => {
+    let i = 0;
+    const interval = setInterval(() => {
+      scrollContainer.current.scrollTo({
+        left: scrollSpy.current[(i++) % scrollSpy.current.length].base.offsetLeft,
+        behavior: "smooth",
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -36,11 +90,15 @@ const Events = () => {
           <h1 style='line-height: 1.5;font-size: 3rem;font-weight: 900;font-family: "Arial Black", Helvetica;'>
             My timezone is{" "}
             <select>
-              {zoneList.map(z => <option>{z}</option>)}
+              {zoneList.map((z) => (
+                <option>{z}</option>
+              ))}
             </select>
             . <br />I want to attend an event that starts at{" "}
             <select>
-              {timeList.map(z => <option>{z}</option>)}
+              {timeList.map((z) => (
+                <option>{z}</option>
+              ))}
             </select>
           </h1>
           <button>9 virtual events found</button>
@@ -48,9 +106,12 @@ const Events = () => {
         </div>
       </div>
       <div class="bg-light text-dark py-5">
-        <div ref={scrollSpy} style={{ overflowX: "scroll", whiteSpace: "nowrap" }}>
-          {eventList.map((e) => (
-            <EventCard event={e} />
+        <div
+          ref={scrollContainer}
+          style={{ overflowX: "scroll", whiteSpace: "nowrap" }}
+        >
+          {eventList.map((e, i) => (
+            <EventCard event={e} ref={(ref) => (scrollSpy.current[i] = ref)} />
           ))}
         </div>
       </div>
