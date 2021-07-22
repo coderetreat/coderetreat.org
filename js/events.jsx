73a57da94dfddc8d5583abffe3c812d5e13eeffe
Matthew Plavcan <mwplavcan@gmail.com>
2021-07-22 12:53:06 -0700

    Added sections to event page filter before/during/after GDCR dates

--- js/events.jsx
+++ js/events.jsx
@@ -7,28 +7,38 @@ import displayEventAsTableRow from "./events/displayEventAsTableRow";
 import interactiveTimeZoneSelector from "./events/interactiveTimeZoneSelector";
 import { LocalizedDate } from "./events/LocalizedDateTime";
 import EventCard from "./events/EventCard";
+import jekyllConfig from '../_config.yml'
 
-const { ZoneId, ZonedDateTime, ChronoUnit } = jsjoda;
+const { ZoneId, ZonedDateTime, LocalDate, ChronoUnit } = jsjoda;
+
+const DATETIME_FORMAT = jsjoda.DateTimeFormatter.ofPattern("u-M-d, HH:mm");
+const earliestGDCRStart = LocalDate.parse(jekyllConfig.globalday.start).atStartOfDayWithZone(ZoneId.of("UTC+12"));
+const latestGDCREnd = LocalDate.parse(jekyllConfig.globalday.end).atStartOfDayWithZone(ZoneId.of("UTC-12")).plusDays(1);
 
 const Events = () => {
   const [timeZone, setTimeZone] = useState(ZoneId.systemDefault().id());
   const timeZoneId = useMemo(() => ZoneId.of(timeZone), [timeZone]);
-  const [events, setEvents] = useState([]);
+  const [{eventsBeforeGDCR, eventsDuringGDCR, eventsAfterGDCR}, setEvents] =
+    useState({eventsBeforeGDCR: [], eventsDuringGDCR: [], eventsAfterGDCR: []});
 
   useEffect(() => {
     const Run = async () => {
       const allEvents = await fetchEventsInChronologicalOrder();
-      setEvents(
-        allEvents.filter((event) => event.date.end.isAfter(ZonedDateTime.now()))
-      );
+      const upcomingEvents = allEvents.filter((event) => event.date.end.isAfter(ZonedDateTime.now()))
+      const eventsBeforeGDCR = upcomingEvents.filter((event) => event.date.start.isBefore(earliestGDCRStart));
+      const eventsDuringGDCR = upcomingEvents.filter((event) =>
+        event.date.start.isAfter(earliestGDCRStart) &&
+        event.date.start.isBefore(latestGDCREnd));
+      const eventsAfterGDCR = upcomingEvents.filter((event) => event.date.start.isAfter(latestGDCREnd));
+      setEvents({eventsBeforeGDCR, eventsDuringGDCR, eventsAfterGDCR});
     };
     Run();
   }, []);
 
   return (
     <div>
-      <div class="container">
-        <h1 class="display-1 my-5">Next Events</h1>
+      <div class="container" style={{minHeight: "max(60vh, 500px)"}}>
+        <h1 class="display-1 my-5 ">Next Events</h1>
         <p class="lead">
           Coderetreats happen all over the world and throughout the whole year!
           Find an event and join your first coderetreat!
@@ -37,8 +47,24 @@ const Events = () => {
           All times shown are in the timezone for{" "}
           {interactiveTimeZoneSelector(timeZone, setTimeZone)}
         </p>
-        <div class="container-fluid p-1" style={{minHeight: "max(60vh, 500px)"}}>
-        {events.map((event) => (
+        <hr/>
+        <h3>Events before Global Day of Coderetreat</h3>
+        <div class="container-fluid p-1">
+        {eventsBeforeGDCR.map((event) => (
+          <EventCard event={event} usersTimezone={timeZoneId} />
+        ))}
+        </div>
+        <hr/>
+        <h3>Global Day of Coderetreat events</h3>
+        <div class="container-fluid p-1">
+        {eventsDuringGDCR.map((event) => (
+          <EventCard event={event} usersTimezone={timeZoneId} />
+        ))}
+        </div>
+        <hr/>
+        <h3>Events after Global Day of Coderetreat</h3>
+        <div class="container-fluid p-1">
+        {eventsAfterGDCR.map((event) => (
           <EventCard event={event} usersTimezone={timeZoneId} />
         ))}
         </div>
