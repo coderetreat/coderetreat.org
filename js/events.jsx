ec4b1a0ecaffed9a73df8ef402893063bdd3ea6d
Raimo Radczewski <raimoradczewski@googlemail.com>
2021-08-05 20:34:45 +0200

    Refactor and clean-up EventList

--- js/events.jsx
+++ js/events.jsx
@@ -3,42 +3,55 @@ import { h, render, Fragment } from "preact";
 import { useEffect, useMemo, useState } from "preact/hooks";
 import "regenerator-runtime/runtime";
 import fetchEventsInChronologicalOrder from "./events/fetchEventsInChronologicalOrder";
-import displayEventAsTableRow from "./events/displayEventAsTableRow";
 import interactiveTimeZoneSelector from "./events/interactiveTimeZoneSelector";
-import { LocalizedDate } from "./events/LocalizedDateTime";
 import EventCard from "./events/EventCard";
-import jekyllConfig from '../_config.yml'
+import jekyllConfig from "../_config.yml";
 
-const { ZoneId, ZonedDateTime, LocalDate, ChronoUnit } = jsjoda;
+const { ZoneId, ZonedDateTime, LocalDate } = jsjoda;
 
-const DATETIME_FORMAT = jsjoda.DateTimeFormatter.ofPattern("u-M-d, HH:mm");
-const earliestGDCRStart = LocalDate.parse(jekyllConfig.globalday.start).atStartOfDayWithZone(ZoneId.of("UTC+12"));
-const latestGDCREnd = LocalDate.parse(jekyllConfig.globalday.end).atStartOfDayWithZone(ZoneId.of("UTC-12")).plusDays(1);
+const earliestGDCRStart = LocalDate.parse(
+  jekyllConfig.globalday.start
+).atStartOfDayWithZone(ZoneId.of("UTC+12"));
+const latestGDCREnd = LocalDate.parse(jekyllConfig.globalday.end)
+  .atStartOfDayWithZone(ZoneId.of("UTC-12"))
+  .plusDays(1);
 const isCurrentDateAfterGDCR = ZonedDateTime.now().isAfter(latestGDCREnd);
 
 const Events = () => {
   const [timeZone, setTimeZone] = useState(ZoneId.systemDefault().id());
   const timeZoneId = useMemo(() => ZoneId.of(timeZone), [timeZone]);
-  const [{eventsBeforeGDCR, eventsDuringGDCR, eventsAfterGDCR}, setEvents] =
-    useState({eventsBeforeGDCR: [], eventsDuringGDCR: [], eventsAfterGDCR: []});
+  const [{ eventsBeforeGDCR, eventsDuringGDCR, eventsAfterGDCR }, setEvents] =
+    useState({
+      eventsBeforeGDCR: [],
+      eventsDuringGDCR: [],
+      eventsAfterGDCR: [],
+    });
 
   useEffect(() => {
     const Run = async () => {
       const allEvents = await fetchEventsInChronologicalOrder();
-      const upcomingEvents = allEvents.filter((event) => event.date.end.isAfter(ZonedDateTime.now()))
-      const eventsBeforeGDCR = upcomingEvents.filter((event) => event.date.start.isBefore(earliestGDCRStart));
-      const eventsDuringGDCR = upcomingEvents.filter((event) =>
-        event.date.start.isAfter(earliestGDCRStart) &&
-        event.date.start.isBefore(latestGDCREnd));
-      const eventsAfterGDCR = upcomingEvents.filter((event) => event.date.start.isAfter(latestGDCREnd));
-      setEvents({eventsBeforeGDCR, eventsDuringGDCR, eventsAfterGDCR});
+      const upcomingEvents = allEvents.filter((event) =>
+        event.date.end.isAfter(ZonedDateTime.now())
+      );
+      const eventsBeforeGDCR = upcomingEvents.filter((event) =>
+        event.date.start.isBefore(earliestGDCRStart)
+      );
+      const eventsDuringGDCR = upcomingEvents.filter(
+        (event) =>
+          event.date.start.isAfter(earliestGDCRStart) &&
+          event.date.start.isBefore(latestGDCREnd)
+      );
+      const eventsAfterGDCR = upcomingEvents.filter((event) =>
+        event.date.start.isAfter(latestGDCREnd)
+      );
+      setEvents({ eventsBeforeGDCR, eventsDuringGDCR, eventsAfterGDCR });
     };
     Run();
   }, []);
 
   return (
     <div>
-      <div class="container" style={{minHeight: "max(60vh, 500px)"}}>
+      <div class="container" style={{ minHeight: "max(60vh, 500px)" }}>
         <h1 class="display-1 my-5 ">Next Events</h1>
         <p class="lead">
           Coderetreats happen all over the world and throughout the whole year!
@@ -48,39 +61,45 @@ const Events = () => {
           All times shown are in the timezone for{" "}
           {interactiveTimeZoneSelector(timeZone, setTimeZone)}
         </p>
-        {isCurrentDateAfterGDCR?<Fragment>
-          <h3>Upcoming Events</h3>
-          <div className="container-fluid p-1">
-            {eventsAfterGDCR.map((event) => (
-              <EventCard event={event} usersTimezone={timeZoneId}/>
-            ))}
-          </div>
-        </Fragment>:<Fragment>
-        <hr/>
-        <h3>Events before Global Day of Coderetreat</h3>
-        <div class="container-fluid p-1">
-        {eventsBeforeGDCR.map((event) => (
-          <EventCard event={event} usersTimezone={timeZoneId} />
-        ))}
-        </div>
-        <hr/>
-        <h3>Global Day of Coderetreat events</h3>
-        <div class="container-fluid p-1">
-        {eventsDuringGDCR.map((event) => (
-          <EventCard event={event} usersTimezone={timeZoneId} />
-        ))}
-        </div>
-        <hr/>
-        <h3>Events after Global Day of Coderetreat</h3>
-        <div class="container-fluid p-1">
-        {eventsAfterGDCR.map((event) => (
-          <EventCard event={event} usersTimezone={timeZoneId} />
-        ))}
-        </div>
-        </Fragment>}
+        <EventList
+          title="Events before Global Day of Coderetreat"
+          events={eventsBeforeGDCR}
+          timeZoneId={timeZoneId}
+        />
+        <EventList
+          title={`Global Day of Coderetreat events (${new Date(
+            jekyllConfig.globalday.start
+          ).toLocaleDateString()} - ${new Date(
+            jekyllConfig.globalday.end
+          ).toLocaleDateString()})`}
+          events={eventsDuringGDCR}
+          timeZoneId={timeZoneId}
+        />
+        <EventList
+          title={
+            isCurrentDateAfterGDCR
+              ? "Upcoming events"
+              : "Events after Global Day of Coderetreat"
+          }
+          events={eventsAfterGDCR}
+          timeZoneId={timeZoneId}
+        />
       </div>
     </div>
   );
 };
+
+const EventList = ({ events, title, timeZoneId }) =>
+  events.length > 0 && (
+    <Fragment>
+      <hr />
+      <h3>{title}</h3>
+      <div class="container-fluid p-1">
+        {events.map((event) => (
+          <EventCard event={event} usersTimezone={timeZoneId} />
+        ))}
+      </div>
+    </Fragment>
+  );
 
 render(<Events />, document.querySelector("#events"));
