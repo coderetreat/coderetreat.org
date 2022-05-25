5e11a5e88bcc2eac0d63af984645ba0477403f8c
Raimo Radczewski <hello@craftswerk.io>
2022-05-25 22:41:26 +0200

    Woop

--- .github/actions/automerger/index.js
+++ .github/actions/automerger/index.js
@@ -1,18 +1,20 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+const octokit = github.getOctokit(GITHUB_TOKEN);
 
-  console.log(github.context.token);
-  console.log(github.context.token.length);
+try {
+  const params = {
+    owner: "coderetreat",
+    repo: "coderetreat.org",
+    pull_number:
+      github.context.payload.number ||
+      github.context.payload.inputs?.pull_request,
+  };
+  console.log({ params });
+  
+  console.log(await octokit.rest.pulls.get(params));
 } catch (error) {
   core.setFailed(error.message);
 }
