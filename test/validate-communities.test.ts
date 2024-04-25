562754d5a41e682952ebd1abe8c8acaa8c9e1e7f
Raimo Radczewski <hello@craftswerk.io>
2024-04-25 21:54:18 +0200

    Add script to create community JSONs from events that have a meetup link

--- test/validate-communities.test.ts
+++ test/validate-communities.test.ts
@@ -5,21 +5,21 @@ import { sync as glob } from "glob";
 import { basename, dirname } from "path";
 
 
-describe("Events", () => {
+describe("Communities", () => {
   test("jest doesn't complain if there's no test defined here yet", () => {});
-  glob(__dirname + "/../_data/events/.SCHEMA.json").forEach((schemaFile) =>
-    describe("Events in " + basename(dirname(schemaFile)), () => {
-      const events = glob(dirname(schemaFile) + "/*.json");
+  glob(__dirname + "/../_data/communities/.SCHEMA.json").forEach((schemaFile) =>
+    describe("Community in " + basename(dirname(schemaFile)), () => {
+      const communities = glob(dirname(schemaFile) + "/*.json");
       const ajv = new Ajv();
       addFormats(ajv);
       const validate = ajv.compile(
         JSON.parse(readFileSync(schemaFile).toString())
       );
 
-      events.forEach((eventFile) =>
-        test(basename(eventFile), () => {
+      communities.forEach((communityFile) =>
+        test(basename(communityFile), () => {
           const result: any = validate(
-            JSON.parse(readFileSync(eventFile).toString())
+            JSON.parse(readFileSync(communityFile).toString())
           );
           if (!result) throw validate.errors;
         })
